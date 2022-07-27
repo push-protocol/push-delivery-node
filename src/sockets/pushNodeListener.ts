f879c4e448c2dd6b8a881c946ed14b0b953f8ebb
Shoaib Mohammed <shoaibmohammed92@gmail.com>
2022-07-27 22:51:32 +0530

    historical feeds related changes

--- src/sockets/pushNodeListener.ts
+++ src/sockets/pushNodeListener.ts
@@ -4,11 +4,35 @@ import {
 const io = require("socket.io-client");
 import feedProcessorService from '../services/feedProcessorService';
 import config from '../config';
+import logger from '../loaders/logger'
 
 export default async () => {
   const feedProcessor = Container.get(feedProcessorService);
-  const ioClient = io.connect(config.PUSH_NODE_WEBSOCKET_URL);
-  ioClient.on("liveFeeds", (feed) => {
+  const socket = io.connect(config.PUSH_NODE_WEBSOCKET_URL);
+  socket.on("liveFeeds", (feed) => {
       feedProcessor.processFeed(feed)
+  });
+
+  var historicalFeedsRequest = {
+      "startTime": 1,
+      "endTime": 1664342399,
+      "page": 1,
+      "pageSize": 10
+  }
+
+  var count = 0
+  socket.emit("historicalFeeds", historicalFeedsRequest);
+  socket.on("historicalFeeds", (data) => {
+      count += data['count']
+      if (data['count'] == 0) {
+          logger.info("Received all the historical data between %o and %o. Total feeds received :: %o. Disconnecting the historicalFeeds socket",  historicalFeedsRequest.startTime, historicalFeedsRequest.endTime, count)
+          socket.disconnect();
+      } else {
+          for(let i = 0; i < data['feeds'].length; i++) {
+            feedProcessor.processFeed(data['feeds'][i])
+          }
+          historicalFeedsRequest.page += 1
+          socket.emit("historicalFeeds", historicalFeedsRequest);
+      }
   });
 }
