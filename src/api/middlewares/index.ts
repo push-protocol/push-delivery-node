be6ec1c983cb5a28aacc2acc41ab8b53e537d3dc
Shoaib Mohammed <shoaibmohammed92@gmail.com>
2022-07-28 08:37:03 +0530

    removing only Authorized code

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -1,9 +1,7 @@
 import onlyLocalhost from './onlyLocalhost'
-import onlyAuthorized from './onlyAuthorized'
 import onlyAuthorizedSimple from './onlyAuthorizedSimple'
 
 export default {
     onlyLocalhost,
-    onlyAuthorized,
     onlyAuthorizedSimple,
 }
