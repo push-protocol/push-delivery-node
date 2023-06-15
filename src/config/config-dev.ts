11962f9317c7d27eb8373b43ccdf725681603d4c
akp111 <pradhanashish95@gmail.com>
2023-06-15 13:57:38 +0530

    add: more examples

--- src/config/config-dev.ts
+++ src/config/config-dev.ts
@@ -4,8 +4,11 @@ export default {
     // channel can be either full caip or no caip
     // if empty, the node will deliver notification for all channels
     // if a channel is in nocaip, it will deliver notification for both the eth and alias channel
+    //ex: 0xabc is a channel that has an alias in polygon. adding 0xabc as an address will allow the delivery node to process
+    // notifications from both the ethereum as well as polygon.
     // if a channel is in caip, it will deliver notification for that particular chain's notification
-    CHANNEL_ADDRESSES: ["abc", "cde"],
+    //ex: eip155:5:0xabc is defined as an address, it will only process notification from ethereum channel and wont process notifications coming from alias
+    CHANNEL_ADDRESSES: [],
 
     /**
      *   Messaging related
