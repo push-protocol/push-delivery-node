dd426ac08d41bbe5afa4be31eb2d108959e40a39
akanshaw-cloudside <141826364+akanshaw-cloudside@users.noreply.github.com>
2024-04-25 23:25:31 +0530

    Update config-staging.ts

--- src/config/config-staging.ts
+++ src/config/config-staging.ts
@@ -1,5 +1,5 @@
 export default {
-    PUSH_NODE_WEBSOCKET_URL: 'https://backend-staging-v2.push.org',
+    PUSH_NODE_WEBSOCKET_URL: 'https://backend-staging.epns.io',
     REDIS_URL: process.env.REDIS_URL,
     REDIS_AUTH: process.env.REDIS_AUTH,
     // channel can be either full caip or no caip
